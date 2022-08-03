27a46d3607a1329b5cc99153ba01934ba58e7f08
kevnnard <londonokevin9@gmail.com>
2022-08-03 14:39:24 -0500

    Deploy 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
@@ -17,10 +18,83 @@ const Alert = forwardRef(function Alert(props, ref) {
 });
 
 function App() {
+
+  // const currencies = [
+  //   {
+  //     value: "Fox Concept Store Plaza central",
+  //     label: "Fox Concept Store Plaza central",
+  //   },
+  //   {
+  //     value: "Fox Concept Store Toberin",
+  //     label: "Fox Concept Store Toberin",
+  //   },
+  //   {
+  //     value: "Fox Concept Store Online",
+  //     label: "Fox Concept Store Online",
+  //   },
+  //   {
+  //     value: "Fox Concept Store Neiva",
+  //     label: "Fox Concept Store Neiva",
+  //   },
+  // ];
+
+  const currencies = [
+    {
+      value: "REPLAYS NUESTRO BOGOTÁ",
+      label: "REPLAYS NUESTRO BOGOTÁ",
+    },
+    {
+      value: "REPLAYS MULTIPLAZA",
+      label: "REPLAYS MULTIPLAZA",
+    },
+    {
+      value: "REPLAYS EDEN",
+      label: "REPLAYS EDEN",
+    },
+    {
+      value: "REPLAYS VILLA DEL RIO",
+      label: "REPLAYS VILLA DEL RIO",
+    },
+    {
+      value: "REPLAYS CALIMA",
+      label: "REPLAYS CALIMA",
+    },
+    {
+      value: "REPLAYS AMERICAS 1",
+      label: "REPLAYS AMERICAS 1",
+    },
+    {
+      value: "REPLAYS AMERICAS 3",
+      label: "REPLAYS AMERICAS 3",
+    },
+    {
+      value: "REPLAYS AMERICAS 5",
+      label: "REPLAYS AMERICAS 5",
+    },
+    {
+      value: "REPLAYS AMERICAS 6",
+      label: "REPLAYS AMERICAS 6",
+    },
+    {
+      value: "REPLAYS AMERICAS 11 OUTLET",
+      label: "REPLAYS AMERICAS 11 OUTLET",
+    },
+    {
+      value: "REPLAYS PLAZA CENTRAL 2",
+      label: "REPLAYS PLAZA CENTRAL 2",
+    },
+    {
+      value: "REPLAYS ANTARES",
+      label: "REPLAYS ANTARES",
+    },
+    {
+      value: "REPLAYS NEIVA",
+      label: "REPLAYS NEIVA",
+    },
+  ];
   // estados
   const [email, setEmail] = useState("");
   const [nombre, setNombre] = useState("");
-  const [cedula, setCedula] = useState("");
   const [telefono, setTelefono] = useState("");
   const [tienda, setTienda] = useState("");
   const [aceptar, setAceptar] = useState(false);
@@ -37,20 +111,23 @@ function App() {
   const handleEnviarFirma = async (e) => {
     e.preventDefault();
     try {
-      if([email,nombre, cedula,telefono,tienda,aceptar].includes("")) {
+      if([email,nombre,telefono,tienda,aceptar].includes("")) {
         return
       } else {
         const { data } = await axios.post(
           `${import.meta.env.VITE_BACKEND_URL}/clienteTerminos`,
-          { email, nombre, cedula, telefono, tienda, aceptar }
+          { email, nombre, telefono, tienda, aceptar }
         );
         setEmail("");
         setNombre("");
-        setCedula("");
         setTelefono("");
         setTienda("");
         setAceptar(false);
         handleClick();
+
+        setTimeout(() => {
+         window.location.replace("https://replays.com.co");
+        }, 3000);
       }
     } catch (error) {
       console.log(error)
@@ -78,20 +155,17 @@ function App() {
             severity="success"
             sx={{ width: "100%" }}
           >
-           !Su Firma a sido exitosa!
+            !Su Firma a sido exitosa!
           </Alert>
         </Snackbar>
       </Stack>
       <div className="contenedor">
-        <div className="contenedor__img">
-          <img src="/img/logo_blanco.png" alt="Fox Racing Colombia" />
-        </div>
         <div className="contenedor__form">
-          <form action="">
+          <form>
             <h1>Tratamientos de datos</h1>
             <Box
               sx={{
-                "& > :not(style)": { m: 1, width: "100%" },
+                "& > :not(style)": { m: 1, width: "100%", height: "100%" },
                 position: "relative",
               }}
               noValidate
@@ -115,15 +189,6 @@ function App() {
                 onChange={(e) => setNombre(e.target.value)}
               />
               <TextField
-                label="Cedula"
-                type="number"
-                required
-                variant="outlined"
-                id="validation-outlined-input"
-                value={cedula}
-                onChange={(e) => setCedula(e.target.value)}
-              />
-              <TextField
                 label="Numero Telefono"
                 type="number"
                 required
@@ -134,13 +199,20 @@ function App() {
               />
               <TextField
                 label="Tienda desde donde aceptas"
+                select
                 required
                 variant="outlined"
                 id="validation-outlined-input"
                 sx={{ borderColor: "#fff" }}
                 value={tienda}
                 onChange={(e) => setTienda(e.target.value)}
-              />
+              >
+                {currencies.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
             </Box>
             <FormGroup>
               <FormControlLabel
@@ -153,20 +225,6 @@ function App() {
                   />
                 }
               />
-              <a
-                style={{
-                  position: "absolute",
-                  bottom: "1rem",
-                  right: 0,
-                  left: 0,
-                  color: "#fff",
-                  textAlign: "center",
-                }}
-                href="https://www.foxracing.com.co/policies/terms-of-service"
-                target="_blank"
-              >
-                <small>ver terminos y condiciones</small>
-              </a>
             </FormGroup>
             <Stack
               direction="row"
@@ -187,6 +245,23 @@ function App() {
             </Stack>
           </form>
         </div>
+        <div className="img">
+          <img src="/img/gruporB.png" alt="" />
+        </div>
+        <a
+          style={{
+            position: "absolute",
+            bottom: ".3rem",
+            right: 0,
+            left: 0,
+            color: "#fff",
+            textAlign: "center",
+          }}
+          href="https://replays.com.co/policies/terms-of-service"
+          target="_blank"
+        >
+          <small>ver terminos y condiciones</small>
+        </a>
       </div>
     </div>
   );
